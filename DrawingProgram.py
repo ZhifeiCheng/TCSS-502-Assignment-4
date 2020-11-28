4e68a8d3fd9c3bf494fb32cff9e9e67df275cea6
deleok <deleok@uw.edu>
2020-11-28 13:33:57 -0800

    Update DrawingProgram.py
    Doc strings added.

--- DrawingProgram.py
+++ DrawingProgram.py
@@ -3,40 +3,57 @@ from DrawingProgramIterator import DrawingProgramIterator
 
 
 class DrawingProgram:
+    """
+    This program holds a list of shapes to be drawn. 
+    The list can be edited by adding, removing, setting, and sorting shapes. 
+    Users can also get a shape at a given index, print shapes, and return the list of shapes.
+    """
+    
+    """DrawingProgram Constructor. Creates an empty list of shapes."""
     def __init__(self):
         self.__shape_list = []
 
+    """Adds a given Shape to the list of shapes."""
     def add_shape(self, shape: Shape):
         self.__shape_list.append(shape)
 
+    """Removes a given shape from the list of shapes."""
     def remove_shape(self, shape: Shape):
         remain = list(filter(lambda x: x != shape, self.__shape_list))
         count = len(self.__shape_list) - len(remain)
         self.__shape_list = remain
         return count
 
+    """Prints a given shape."""
     def print_shape(self, target_shape: Shape):
         for shape in self.__shape_list:
             if shape.name == target_shape.name:
                 shape.draw()
                 shape.draw_graphic()
 
+    """Returns the shape that is in the list at the given index."""
     def get_shape(self, index: int) -> Shape:
         if len(self.__shape_list) > 0 and 0 <= index < max(len(self.__shape_list), 1):
             return self.__shape_list[index]
         else:
             raise ValueError("index is out of range")
 
+    """Sets the given shape to the given index in the list."""
     def set_shape(self, index: int, shape: Shape):
         if len(self.__shape_list) > 0 and 0 <= index < len(self.__shape_list):
             self.__shape_list[index] = shape
         else:
             raise ValueError("index is out of range")
 
+    """
+    Sorts shapes in the list. 
+    Shapes will be sorted first by name, then by area if names are same.
+    """
     def sort_shape(self):
         DrawingProgram.merge_sort(self.__shape_list)
 
     @staticmethod
+    """Merge sort used to sort the shapes."""
     def merge_sort(lst):
         if len(lst) > 1:
             mid = len(lst) // 2
@@ -63,9 +80,11 @@ class DrawingProgram:
                 lst[k] = right[j]
                 j += 1
                 k += 1
-
+    
+    """Returns the strings of the shapes in the list."""
     def __str__(self):
         return "\n".join(map(lambda shape: shape.get_name, self.__shape_list))
 
+    """Returns the list."""
     def __iter__(self):
         return DrawingProgramIterator(self.__shape_list)
